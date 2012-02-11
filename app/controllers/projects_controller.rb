d314479d8a297a9803991413772299c233df84ad
Marcelo G. Silva <mgswolf@gmail.com>
2012-02-11 16:47:42 -0200

    deleting projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -35,4 +35,11 @@ class ProjectsController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
+  end
 end
--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -26,3 +26,7 @@ end
 Given /^there is a project called "([^"]*)"$/ do |name|
   Factory(:project, :name => name)
 end
+
+Then /^I should not see "([^"]*)"$/ do |text|
+   page.should_not have_content(text)
+end
