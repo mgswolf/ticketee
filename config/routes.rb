9c03565fe2299905b73695ebd08d2773b0784665
Marcelo G. Silva <mgswolf@gmail.com>
2012-02-19 19:08:10 -0200

    Added admin namespaced users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Ticketee::Application.routes.draw do
+
   devise_for :users
 
   resources :projects do
@@ -50,6 +51,10 @@ Ticketee::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  namespace :admin do
+    resources :users
+  end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
