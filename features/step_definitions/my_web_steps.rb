e824f058d7fd06ae1f1dacc78b7e704eaaa07e5e
Marcelo G. Silva <mgswolf@gmail.com>
2012-02-19 19:57:29 -0200

    Added the ability to create admin users through the admin backend

---
+++ features/step_definitions/my_web_steps.rb
@@ -0,0 +1,35 @@
+Given /^I am on the homepage$/ do
+  visit root_path
+end
+
+When /^I follow "([^"]*)"$/ do |link|
+  click_link(link)
+end
+
+When /^I fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  fill_in(field, :with => value)
+end
+
+When /^I press "([^"]*)"$/ do |button|
+  click_button(button)
+end
+
+Then /^I should see "([^"]*)"$/ do |text|
+  page.should have_content(text)
+end
+
+Then /^I should be on the project page for "([^"]*)"$/ do |name|
+  current_path = URI.parse(current_url).path
+  current_path.should == project_path(Project.find_by_name!(name))
+end
+
+
+Then /^I should not see "([^"]*)"$/ do |text|
+  page.should have_no_content(text)
+end
+
+When /^I check "([^"]*)"$/ do |field|
+  check(field)
+end
+
+
--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -1,32 +1,3 @@
-Given /^I am on the homepage$/ do
-  visit root_path
-end
-
-When /^I follow "([^"]*)"$/ do |link|
-  click_link(link)
-end
-
-When /^I fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
-  fill_in(field, :with => value)
-end
-
-When /^I press "([^"]*)"$/ do |button|
-  click_button(button)
-end
-
-Then /^I should see "([^"]*)"$/ do |text|
-  page.should have_content(text)
-end
-
-Then /^I should be on the project page for "([^"]*)"$/ do |name|
-  current_path = URI.parse(current_url).path
-  current_path.should == project_path(Project.find_by_name!(name))
-end
-
 Given /^there is a project called "([^"]*)"$/ do |name|
   @project = Factory(:project, :name => name)
-end
-
-Then /^I should not see "([^"]*)"$/ do |text|
-   page.should have_no_content(text)
 end
