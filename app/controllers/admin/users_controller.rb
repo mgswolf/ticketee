69f74bedd79a7cbfe502f77cc8509967b694d4de
Marcelo G. Silva <mgswolf@gmail.com>
2012-02-19 21:48:38 -0200

    added ability to edit and update users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,15 @@
 class Admin::UsersController < Admin::BaseController
+  before_filter :find_user, :only => [:show, :edit, :update,:destroy]
   def index
     @users = User.all(:order => "email")
+  end
+
+  def show
+
+  end
+
+  def edit
+
   end
 
   def new
@@ -9,7 +18,7 @@ class Admin::UsersController < Admin::BaseController
 
   def create
     @user = User.new(params[:user])
-    @user.admin = params[:user][:admin] == "1"
+    set_admin
     if @user.save
       flash[:notice] = "User has been created."
       redirect_to admin_users_path
@@ -19,4 +28,27 @@ class Admin::UsersController < Admin::BaseController
     end
   end
 
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+    set_admin
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "User has been updated."
+      redirect_to admin_users_path
+    else
+      flash[:alert] = "User has not been updated."
+      render "edit"
+    end
+  end
+
+  private
+   def find_user
+     @user = User.find(params[:id])
+   end
+
+  def set_admin
+    @user.admin = params[:user][:admin] == "1"
+  end
 end
