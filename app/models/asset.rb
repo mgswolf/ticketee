e869df214a23ef9437f485ce91a0a695210d7a2d
Marcelo G. Silva <mgswolf@gmail.com>
2012-02-24 14:48:44 -0200

    Assets are now strictly through FilesController

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,4 +1,5 @@
 class Asset < ActiveRecord::Base
   belongs_to :ticket
-  has_attached_file :asset
+  has_attached_file :asset, :path => (Rails.root + "files/:id").to_s,
+                            :url => "/files/:id"
 end
